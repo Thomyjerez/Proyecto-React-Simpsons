9ddc329c45310f10fb9cdb0c5dc84c353bc0227b
thomas jerez <tjerez@outlook.com>
2022-10-03 18:59:23 -0300

    agregamos la consulta de api

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Container} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import Frase from './components/Frase';
 import './style.css';
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
+import { useState } from "react";
 
 function App() {
+  // para pedir la frase y guardarla en un state para que redibuje automaticamente
   const [personaje, setPersonaje] = useState({});
 
-useEffect(()=>{
+// siempre hacer la peticion a la api en montaje si usamos ciclo de vida
+useEffect (()=>{
   consultarAPI();
 },[])
 
-const consultarAPI = async() =>{
-  
+const consultarAPI =async()=>{
+  // estructura para controlar errores, siempre poner el fetch dentro de esta estructura
   try {
-      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
-      console.log(respuesta);
-      const dato = await respuesta.json();
-      console.log(dato[0]);
-      setPersonaje(dato[0]);
-      
-    } catch (error) {
-       console.log(error);
-      //  mostrar un mensaje al usuario
-    }
-}
+    // herramienta para hacer peticiones a una api
+    // de parametro el url de la api
+    // await se utiliza por el tiempo que tarda en hacer la peticion la api, 
+    // utilizamos el async (asincronico) y el await
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+    // console.log(respuesta)
+    const dato = await respuesta.json();
+    // console.log(dato[0])
+    // para guardar el dato en el state
+    setPersonaje(dato[0])
+    
+    
+  } catch (error) {
+    console.log(error)
+    // mostrar un msj al usuario que ocurrio un error
+  }
 
+}
   return (
     <Container>
       <div className="m-5 text-center">
@@ -36,7 +45,7 @@ const consultarAPI = async() =>{
       />
       </div>
       <div className="text-center">
-      <Button className="botonNaranja mb-5" variant='warning' onClick={consultarAPI}>Obtener frase</Button>
+      <Button className="botonNaranja mb-5" variant='warning'>Obtener frase</Button>
       </div>
       <Frase personaje={personaje}></Frase>
     </Container>
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,33 +1,45 @@
 import { Card, Row, Col } from "react-bootstrap";
 
-const Frase = ({personaje}) => {
+const Frase = () => {
   return (
     <Card>
       <Card.Body>
       <Row>
         <Col md={4}>
           <img
-            src={personaje.image}
-            alt={personaje.character}
+            src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd86034034e0170124eec/previews/632cd86134034e0170124ef2/download/image.png"
+            alt="Homero simpson"
             className="w-100"
-          />
+            />
         </Col>
         <Col md={8}>
           <Card.Title>
-            <h3>{personaje.character}</h3>
+            <h3>Homero simpson</h3>
           </Card.Title>
           <Card.Text>
-           {personaje.quote}
+            Facts are meaningless. You could use facts to prove anithing thats
+            even remotely true.
           </Card.Text>
         </Col>
       </Row>
-      </Card.Body>
+            </Card.Body>
     </Card>
+
     // <section className="row bg-white my-5 p-5 m-1">
     //   <aside className="col-4">
+    //     <img
+    //       src="https://trello.com/1/cards/632a448eb47fe800d9bdef25/attachments/632cd86034034e0170124eec/previews/632cd86134034e0170124ef2/download/image.png"
+    //       alt="Homero simpson"
+    //       className="w-100"
+    //     />
     //   </aside>
     //   <aside className="col-8">
     //     <div>
+    // <h3>Homero simpson</h3>
+    // <p className="fs-5">
+    //   Facts are meaningless. You could use facts to prove anithing thats
+    //   even remotely true.
+    // </p>
     //     </div>
     //   </aside>
     // </section>
